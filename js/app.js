e592c61c834ff76cdfe0ace1734fe2fe5ae342ad
munnisaikra <munnisaikra750@gmail.com>
2022-09-03 20:55:15 +0600

    display data

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
-
- 
 const fetchCategory = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-    .then(res=>res.json())
-    .then(data=>loadCategory(data.data.news_category))
-
-}
+  fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then((res) => res.json())
+    .then((data) => loadCategory(data.data.news_category));
+};
 fetchCategory();
 
 const loadCategory = (data) => {
-    const category = document.getElementById('category');
-    data.forEach(item => {
-        const span = document.createElement('span');
-        span.classList.add("text-[18px]","text-[#858585]","cursor-pointer")
-        span.innerText = item.category_name;
-console.log(item);
-        category.appendChild(span);
-         
-    })
-}
+  const category = document.getElementById("category");
+  data.forEach((item) => {
+    console.log(item);
+    const span = document.createElement("span");
+    span.classList.add("text-[18px]", "text-[#858585]", "cursor-pointer");
+    span.innerHTML = `<span>${item.category_name}
+        </span`;
+    span.addEventListener("click", () => {
+      displayCategoryData(item.category_id);
+    });
+    category.appendChild(span);
+  });
+};
+
+// display category data
+const displayCategoryData = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  console.log(id);
+};
+const itemResult = document.getElementById("items-result");
+ 
