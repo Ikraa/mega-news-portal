b6e0c463073d2f30b442a3c9fe6374e00a3d192e
munnisaikra <munnisaikra750@gmail.com>
2022-09-04 04:07:47 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ const loadCategory = (data) => {
         </span`;
     span.addEventListener("click", () => {
       displayCategoryData(item.category_id,item.category_name);
+      document.getElementById("card-container").innerHTML=""
     });
     category.appendChild(span);
   });
@@ -32,7 +33,7 @@ const displayCategoryData = (id,name) => {
 
 const itemsResult =(items,name)=>{
   const itemResult = document.getElementById("items-result");
-  console.log(itemResult,"ikra") 
+ 
   console.log(items.data.length,name) 
   itemResult.innerHTML=`${items?.data?.length} items found for category ${name}`
   displayCategoryCard(items.data)
@@ -43,31 +44,39 @@ const itemsResult =(items,name)=>{
 //
 const displayCategoryCard=(data)=>{
  const cardContainer=document.getElementById("card-container")
+ 
+
+ 
  data.map(card=>{
   console.log(card)
          const div=document.createElement('div')
+         console.log(typeof card._id)
          div.innerHTML=`
                   <div class="card mb-[25px] lg:card-side p-5 bg-[#FFFFFF]  shadow-xl">
               <img class="h-[300px] w-[244px] object-cover mr-[40px]" src=${card.image_url} alt="Album">
             <div class="card-body">
               <h2 class="card-title text-[#121221] font-bold text-[24px]">${card.title}</h2>
-              <p class="text-[16px] my-4 text-[#949494] whitespace-pre-line">${card.details}</p>
+              <p class="text-[16px] text-ellipsis  my-4 text-[#949494] whitespace-pre-line">${card.details.slice(0,500)}${card.details.length>500?"...":""}</p>
               
             <div class="footerWraper flex items-center justify-between">
                 <div class="flex items-center">
                 <img class="h-[40px] w-[40px] rounded-full mr-[10px]" src=${card?.author?.img} /> 
                 <div> 
-                      <h1 class="text-[#2B2C34] text-[16px]">Name</h1>
-                      <h1 class="text-[14px] text-[#718797]">jan 10 ,2020</h1>
+                      <h1 class="text-[#2B2C34] text-[16px]">${card?.author?.name}</h1>
+                      <h1 class="text-[14px] text-[#718797]">${new Date(card?.author?.published_date).toDateString()}</h1>
                 </div>
                 </div>
 
-                 <div class="text-[#515151] text-[18px] font-bold flex items-center"> <span><img class="h-[24px] mr-[12px] w-[24px]" src="./img/eye-icon.png" /></span><span class="text-[#515151] text-[18px] font-bold">1.5m</span> </div>
-                 <div>
-                 <img src="./img/right-arrow.png" />
+                 <div class="text-[#515151] text-[18px] font-bold flex items-center"> <span><img class="h-[24px] mr-[12px] w-[24px]" src="./img/eye-icon.png" /></span><span class="text-[#515151] text-[18px] font-bold">${card.total_view}</span> </div>
+                 <div >
+                 <label onClick="openModal('${card?._id}')" for="my-modal-6" class="btn modal-button">
+                 <img  class="mr-[14px]" src="./img/right-arrow.png" />
+                 </label>
+                 
                  </div>
             </div>
-            </div>
+            
+            </div> 
           </div>
          
          `
@@ -77,3 +86,46 @@ const displayCategoryCard=(data)=>{
 
  })
 }
+
+const openModal=(news_id)=>{
+const url=`https://openapi.programming-hero.com/api/news/${news_id}`
+fetch(url)
+.then(res=>res.json())
+.then(data=>loadModalData(data?.data[0]))
+
+}
+
+
+const loadModalData=(news)=>{
+  const {category_id,details,image_url,thumbnail_url,title,total_view}=news
+  const {name,published_date,img}=news.author
+  const {is_trending}=news.others_info
+  const newsContainer=document.getElementById("modal-container").innerHTML=`
+  <div class="flex items-center justify-between">
+
+  <div> 
+  <h1 class="text-xs font-bold text-black">Category Id:${category_id} </h1>
+  <h1 class="text-xs text-gray-600 ">Trending:${is_trending?"yes":"no"} </h1>
+  </div>
+  
+  <div>
+  <h1 class="text-xs font-bold text-black">Author:${name} </h1>
+  <h1 class="text-xs text-gray-600">Published Date:${new Date(published_date).toDateString()} </h1>
+  </div>
+  <img class="h-[24px] w-[24px] rounded-full" src="${img}" />
+  
+  </div>
+  <img class="h-[35vh] my-3 w-[100%] object-cover mx-auto" src="${thumbnail_url}" />
+  <h1 class="text-gray-500"> ${details}</h1>
+
+
+  <div class="modal-action">
+  <label for="my-modal-6" class="btn">Close!</label>
+</div>
+  
+  
+  `
+}
+
+
+
