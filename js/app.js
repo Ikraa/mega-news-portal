abd44f5e4e2756d00fbb953d6fdda5c4a32bb7dc
munnisaikra <munnisaikra750@gmail.com>
2022-09-03 17:15:04 +0600

    added items result section

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
-const category = document.getElementById('category');
+
  
 const fetchCategory = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res=>res.json())
-    .then(data=>console.log(data.data.news_category))
+    .then(data=>loadCategory(data.data.news_category))
 
 }
 fetchCategory();
 
+const loadCategory = (data) => {
+    const category = document.getElementById('category');
+    data.forEach(item => {
+        const span = document.createElement('span');
+        span.classList.add("text-[18px]","text-[#858585]","cursor-pointer")
+        span.innerText = item.category_name;
+console.log(item);
+        category.appendChild(span);
+         
+    })
+}
